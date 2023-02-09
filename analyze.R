03fefa7050c0bb3c5278c84203ee6e9c6ca2828f
oh80 <ogawa-hajime-vs@ynu.jp>
2023-02-09 15:39:18 +0900

    initial commit

--- analyze.R
+++ analyze.R
@@ -7,9 +7,11 @@ main <-function(){
   probit_model <- data %>% probit_reg()
   linear_model <- data %>% linear_reg()
   
-  #限界効果と閾値パラメータの推定を行う予定
+  marginal_effect_rable <- get_values(probit_model,linear_model) %>% 
+    get_table()
   
-  return(probit_model)
+  
+  marginal_effect_rable %>% save_table()
 }
 
 
@@ -26,7 +28,34 @@ linear_reg <- function(input){
   return(output)
 }
 
-pm <- main()
+
+get_values <- function(input1,input2){
+  linear_maginal_effect <- input2$coefficients[2]
+  
+  probit_hidden_param <- input1$coefficients[2]
+  probit_coef <- dnorm(probit_hidden_param,
+                   mean = 0, sd = 1)*probit_hidden_param 
+  probit_maginal_effect <- mean(probit_coef)
+  
+  marginal_effects <- c(linear_maginal_effect,probit_maginal_effect)
+  return(marginal_effects)
+}
 
 
+get_table <- function(input){
+  output <- kableExtra::kable(t(input),
+                              digits = 5,
+                              col.names = c("linear model","probit model"),
+                              format = "latex")
+  return(output)
+}
+
+
+save_table <- function(input){
+  path <- "02_analyze/probit_regression/output/table.txt"
+  writeLines(input,path)
+}
+
+
+main()
 
