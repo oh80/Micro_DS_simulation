a79d9c6ba1e98d42090951b85099983c4b70b0c9
oh80 <ogawa-hajime-vs@ynu.jp>
2023-02-10 09:44:33 +0900

    modify probit marginal

--- analyze.R
+++ analyze.R
@@ -4,12 +4,11 @@
 main <-function(){
   data <- readRDS(file = "01_data/data/data.obj")
   
-  probit_model <- data %>% probit_reg()
+  probit_model <- data %>% probit_reg() 
   linear_model <- data %>% linear_reg()
   
-  marginal_effect_rable <- get_values(probit_model,linear_model) %>% 
+  marginal_effect_rable <- data %>% get_values(probit_model,linear_model) %>% 
     get_table()
-  
   
   marginal_effect_rable %>% save_table()
 }
@@ -29,15 +28,15 @@ linear_reg <- function(input){
 }
 
 
-get_values <- function(input1,input2){
-  linear_maginal_effect <- input2$coefficients[2]
+get_values <- function(input_data,input_model1,input_model2){
+  linear_maginal_effect <- input_model2$coefficients[2]
   
-  probit_hidden_param <- input1$coefficients[2]
-  probit_coef <- dnorm(probit_hidden_param,
+  probit_hidden_param <- input_model1$coefficients[2]
+  probit_marginal <- dnorm(probit_hidden_param*input_data$physical_ability,
                    mean = 0, sd = 1)*probit_hidden_param 
-  probit_maginal_effect <- mean(probit_coef)
+  probit_maginal_mean <- mean(probit_marginal)
   
-  marginal_effects <- c(linear_maginal_effect,probit_maginal_effect)
+  marginal_effects <- c(linear_maginal_effect,probit_maginal_mean)
   return(marginal_effects)
 }
 
@@ -58,4 +57,3 @@ save_table <- function(input){
 
 
 main()
-
